50b1277be7176f5bd1703cf83508960d1e3c22e4
AdrianBellmunt <adrianbellmuntorero@gmail.com>
2022-12-16 13:49:58 +0100

    plantilla _team.html.twig creada

--- symfony-tiendaPersonal/src/Controller/PageController.php
+++ symfony-tiendaPersonal/src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('page/index.html.twig', []);
+        $repository = $doctrine->getRepository(Team::class);
+        $team = $repository->findAll();
+        return $this->render('page/index.html.twig', compact('team'));
     }
-
+    
     #[Route('/about', name: 'about')]
     public function about(): Response
     {
