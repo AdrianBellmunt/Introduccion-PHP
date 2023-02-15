9206caede539bf83cfce29daecfe099ac5f2d61f
AdrianBellmunt <adrianbellmuntorero@gmail.com>
2023-02-15 09:37:18 +0100

    Parte 4 retocada y acabada

--- Node/express-locallibrary-tutorial/models/genre.js
+++ Node/express-locallibrary-tutorial/models/genre.js
@@ -1,11 +1,13 @@
+var mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const genre = new Schema({
+const GenreSchema = new Schema({
   name: { type: String, maxLength:100, minLength:3 },
 });
 
 // Virtual for bookinstance's URL
-BookInstanceSchema.virtual("url").get(function () {
+GenreSchema.virtual("url").get(function () {
     // We don't use an arrow function as we'll need the this object
     return `/catalog/genre/${this._id}`;
   });
